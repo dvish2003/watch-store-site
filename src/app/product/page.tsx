2fcfaa276f402a398b4fc8277f508039e9c60474
Tashini4 <tashiniwijethunga@gmail.com>
2025-11-03 13:43:50 +0530

    ddd

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,7 +4,7 @@ const ProductPage = () => {
   return (
     <section className="h-screen w-full flex items-center justify-center">
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-black text-center max-h-dvh">
-        Welcome to the 
+        We
       </h1>
     </section>
   );
